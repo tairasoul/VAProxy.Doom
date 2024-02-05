86c350e4565990f2f8a7ac7cfd05fb1b31fae65d
tairasoul <mjrcoetzee+1@outlook.com>
2024-02-05 20:22:30 +0200

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+
+namespace DoomInProxy
+{
+    internal class PluginInfo
+    {
+        internal const string Name = "DoomIn.VAProxy";
+        internal const string GUID = "tairasoul.vaproxy.doom";
+        internal const string Version = "1.0.0";
+    }
+
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static Plugin Instance;
+        internal bool init = false;
+        void Awake()
+        {
+            Instance = this;
+            Logger.LogInfo($"Plugin {PluginInfo.GUID} ({PluginInfo.Name}) version {PluginInfo.Version} loaded.");
+        }
+
+        void Start() => Init();
+        void OnDestroy() => Init();
+
+        void Init()
+        {
+            if (init) return;
+            init = true;
+        }
+    }
+}
